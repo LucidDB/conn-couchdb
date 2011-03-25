ad66a1bce60afe4eef990f0c166f8e3a85e0a62c
Kevin Secretan <ksecretan@dynamobi.com>
2011-03-25 22:20:59 +0000

    Using commons Base64, added licensing, creates a jar with everything in it.

--- src/com/dynamobi/db/conn/couchdb/CouchDBdcUdx.java
+++ src/com/dynamobi/db/conn/couchdb/CouchDBdcUdx.java
@@ -1,6 +1,21 @@
-/**
- * Copyright DynamoBI Corporation
- */
+/*
+Dynamo CouchDB Connector is a plugin for browsing CouchDB views in LucidDB.
+Copyright (C) 2011 Dynamo Business Intelligence Corporation
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option)
+any later version approved by Dynamo Business Intelligence Corporation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 package com.dynamobi.db.conn.couchdb;
 
@@ -21,45 +36,62 @@ import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import sun.misc.BASE64Encoder;
+import org.apache.commons.codec.binary.Base64;
 
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
+/**
+ * Generic Udx class to build any custom functions to browse CouchDB views.
+ */
 
 public class CouchDBdcUdx {
 
   /**
-   * @param resultInserter - assumed to have the necessary column
-   * names in the order we get them.
+   * Called by a custom LucidDB function for each view.
+   * @param userName - CouchDB user name
+   * @param pw - CouchDB password
+   * @param url - CouchDB REST URL
+   * @param view - CouchDB REST view -- concatenated on the end of URL with
+   *               a slash prefix.
+   * @param resultInserter - Table for inserting results. Assumed to have the
+   * necessary column names in the order we get them.
    */
   public static void execute(
-      String user,
+      String userName,
       String pw,
-      String view,
       String url,
+      String view,
       PreparedStatement resultInserter) {
-    JSONArray lst = getViewRows(user, pw, view, url);
+
+    JSONArray lst = getViewRows(userName, pw, url, view);
+    if (lst == null)
+      return;
+
     try {
-      for (JSONObject obj : lst) {
-        JSONObject cols = obj.get("value");
+      Iterator iter = lst.iterator();
+      while (iter.hasNext()) {
+        JSONObject obj = (JSONObject) iter.next();
+        JSONObject cols = (JSONObject) obj.get("value");
         int c = 0;
-        for (Iterator iter = cols.keySet().iterator(); iter.hasNext(); ) {
-          String key = (String) iter.next();
+        for (Iterator iter2 = cols.keySet().iterator(); iter2.hasNext(); ) {
+          String key = (String) iter2.next();
           String value = (String) cols.get(key);
           resultInserter.setString(++c, value);
         }
         resultInserter.executeUpdate();
       }
     } catch (SQLException e) {
-      throw ApplibResource.instance().DatabaseAccessError.ex(
-          e.toString(),
-          e);
+      e.printStackTrace();
     }
   }
   
-  private static void getViewRows(
-      String user, String pw, String view, String url) {
+  /**
+   * Helper function gets the rows from the foreign CouchDB server and returns
+   * them as a JSONArray.
+   */
+  private static JSONArray getViewRows(
+      String user, String pw, String url, String view) {
     try {
       URL u = new URL(url + "/" + view);
       HttpURLConnection uc = (HttpURLConnection) u.openConnection();
@@ -83,15 +115,19 @@ public class CouchDBdcUdx {
     return null;
   }
   
+  /**
+   * Builds a standard basic authentication http header with the user and pass.
+   */
   private static String buildAuthHeader (String username, String password) {
     String login = username + ":" + password;
-    String encodedLogin = new BASE64Encoder().encode(login.getBytes());
+    String encodedLogin = new String(Base64.encodeBase64(login.getBytes()));
     return encodedLogin;
-    
   }
   
+  /**
+   * Reads the data from a http call into a string.
+   */
   private static String readStringFromConnection (HttpURLConnection uc) throws IOException {
-    
     InputStreamReader in = new InputStreamReader(uc.getInputStream());
     BufferedReader buff = new BufferedReader(in);
     StringBuffer sb = new StringBuffer();
@@ -103,13 +139,5 @@ public class CouchDBdcUdx {
     
     return sb.toString();
   }
-  
-  public static void main(String [ ] args) {
-    
-    getStaticView();
-    
-    
-  }
-
   
 }
